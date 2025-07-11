f7da54519ec573f14e6538f1d534596048358528
João Gabriel <70417853+ojgabrielleal@users.noreply.github.com>
2025-07-11 20:44:22 -0300

    Update index.js

--- resources/js/Components/Card/index.js
+++ resources/js/Components/Card/index.js
@@ -1,3 +1,4 @@
 export { default as Alert } from "./Alert.svelte"
 export { default as Task } from "./Task.svelte"
 export { default as Post } from "./Post.svelte"
+export { default as Calendar } from "./Calendar.svelte"
