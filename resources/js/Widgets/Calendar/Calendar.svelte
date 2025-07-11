66f94c2752c610145526a89edd7e7e2adfcc834e
João Gabriel <70417853+ojgabrielleal@users.noreply.github.com>
2025-07-11 20:43:35 -0300

    Update Calendar.svelte

--- resources/js/Widgets/Calendar/Calendar.svelte
+++ resources/js/Widgets/Calendar/Calendar.svelte
@@ -1,13 +1,70 @@
 <script>
     export let title; 
 
+    import { router, page } from "@inertiajs/svelte";
+
     import { Section } from "@/Layouts";
+    import { Calendar } from "@/Components/Card"
+
+    $:({ calendar } = $page.props);
+
+    // Tags from calendar
+    const tags = [
+        { label: 'Programas', color: 'var(--color-blue-skywave)' },
+        { label: 'Lives', color: 'var(--color-purple-mystic)' },
+        { label: 'Youtube', color: 'var(--color-red-crimson)' },
+        { label: 'Podcasts', color: 'var(--color-green-forest)' },
+        { label: '', color: 'var(--color-blue-skywave)' },
+        { label: '', color: 'var(--color-blue-skywave)' },
+        { label: '', color: 'var(--color-blue-skywave)' },
+    ];
+
+    // Weekdays labels and items
+    let week = [];
+    $: if(calendar){
+        console.log(calendar)
+
+        const labels = {
+            sun: "Dom",
+            mon: "Seg",
+            tue: "Ter",
+            wed: "Qua",
+            thu: "Qui",
+            fri: "Sex",
+            sat: "Sáb",
+        };
+
+        week = Object.keys(labels).map((key) => ({
+            day: labels[key],
+            items: calendar[key] || [],
+        }));
+    }
 </script>
 
-<Section title={title}>
-    <div class="flex justify-center items-center h-[20rem]">
-        <div class="text-2xl text-gray-500">
-            Calendar widget is under construction.
+{#if Object.keys(calendar || {}).length > 0}
+    <Section title={title}>
+        <div class="w-full grid gap-5 grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
+                {#each tags as tag}
+                    <span class="h-10 text-[var(--color-neutral-aurora)] text-lg font-noto-sans font-bold uppercase italic rounded-lg flex justify-center items-center" style="background-color: {tag.color}">
+                        {tag.label}
+                    </span>
+                {/each}
+            </div>
+            <div class="w-full grid gap-5 mt-5 grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
+            {#each week as dayBlock}
+                <div class="flex flex-col gap-3 w-full" id={dayBlock.id}>
+                    <span class="text-[var(--color-neutral-aurora)] text-lg font-noto-sans text-center font-bold uppercase italic">
+                        {dayBlock.day}
+                    </span>
+                    {#if dayBlock.items.length === 0}
+                        <Calendar />
+                    {:else}
+                        {#each dayBlock.items as item}
+                            <Calendar item={item} category={item.category} />
+                        {/each}
+                    {/if}
+                </div>
+            {/each}
         </div>
-    </div>
-</Section>
+    </Section>
+{/if}   
