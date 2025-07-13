8808d2f5506b81fd5e3f113437e5d5b72354ab22
ojgabrielleal <joaogabrieel79@gmail.com>
2025-07-12 22:47:38 -0300

    feat: add analytics component with icons for kbps, satellite, and listeners

--- resources/js/layouts/admin/Layout.svelte
+++ resources/js/layouts/admin/Layout.svelte
@@ -3,6 +3,8 @@
     import { toast, Toaster } from "svelte-hot-french-toast";
 
     import { Navbar } from "@/widgets/admin/navbar";
+    import { Analytics } from "@/widgets/admin/analytics";
+
     import NavbarItems from "@/data/admin/Navbar";
 
     $: ({ user } = $page.props);
@@ -12,16 +14,22 @@
     $: if(flash){
         toast[flash.type](flash.message);
     }
+
+    // Define background color used in the layout
+    document.body.style.backgroundColor = "var(--color-blue-indigo)";
 </script>
 
 <Toaster position="bottom-end" />
-<div class="w-full bg-[var(--color-blue-indigo)]">
-    <header class="lg:pt-10">
-        <Navbar items={NavbarItems} user={user}/>
-        <slot name="header" />
-    </header>
-    <main class="container">
-        <slot />
-    </main>
-</div>
-
+<header class="lg:pt-10">
+    <Navbar items={NavbarItems} user={user}/>
+    <slot name="header" />
+</header>
+<main class="container">
+    <slot />
+</main>
+<footer>
+    <div class="h-[5rem]"></div>
+    <div class="w-full fixed bottom-0">
+        <Analytics/>
+    </div>
+</footer>
---
+++ resources/js/widgets/admin/analytics/Analytics.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Icon from "@iconify/svelte";
+</script>
+
+<section class="container border-t border-[var(--color-orange-amber)] bg-[var(--color-blue-indigo)] py-4">
+    <div class="flex">
+        <span class="flex gap-2 items-end font-noto-sans text-[var(--color-orange-amber)] text-xl uppercase pr-6 border-r border-r-[rgba(229,231,235,0.3)]">
+            <img src="/icons/kbps.svg" alt="kpbs icon" class="w-8 color-filter-blue-skywave" />
+            320 KBPS
+        </span>
+        <span class="flex gap-2 items-end font-noto-sans text-[var(--color-orange-amber)] text-xl uppercase pl-6 pr-6 border-r border-r-[rgba(229,231,235,0.3)]">
+            <img src="/icons/satelite.svg" alt="satelite icon" class="w-8 color-filter-blue-skywave" />
+            ONLINE
+        </span>
+        <span class="flex gap-2 items-end font-noto-sans text-[var(--color-orange-amber)] text-xl uppercase pl-6 pr-6">
+            <img src="/icons/listeners.svg" alt="listeners icon" class="w-8 color-filter-blue-skywave" />
+             20 OUVINTES
+        </span>
+    </div>
+</section>
