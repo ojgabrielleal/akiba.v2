03e85826ece41d1176a823e5d659b08652a89471
ojgabrielleal <joaogabrieel79@gmail.com>
2025-10-17 12:42:20 -0300

    fix: update view property for better responsiveness and improve image alt attribute

--- resources/js/components/admin/Preview.svelte
+++ resources/js/components/admin/Preview.svelte
@@ -1,7 +1,7 @@
 <script>
     export let name = null;
     export let size = "w-full h-[15rem] lg:h-[23rem]";
-    export let view = "w-full h-[15rem] lg:h-[23rem]";
+    export let view = "w-full max-h-[15rem] lg:max-h-[23rem]";
     export let src = null;
     export let oninput = null;
 
@@ -25,7 +25,7 @@
 
 <label class="cursor-pointer">
     {#if imageToShow}
-        <img src={imageToShow} alt="Preview de imagem" class={`${view} rounded-lg object-contain object-top`} loading="lazy"/>
+        <img src={imageToShow} alt="" aria-hidden="true" class={`${view} rounded-lg object-contain object-top`} loading="lazy"/>
     {:else}
         <div class={`${size} bg-neutral-aurora rounded-lg flex items-center justify-center overflow-hidden font-noto-sans text-blue-skywave text-7xl font-bold italic uppercase`}>
             +
