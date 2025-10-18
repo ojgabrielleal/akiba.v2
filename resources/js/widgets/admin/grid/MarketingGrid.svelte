71ce607417818d5f0c5a2a23309001ca2a20484e
ojgabrielleal <joaogabrieel79@gmail.com>
2025-10-17 23:15:58 -0300

    fix: rename section title and adjust layout for better content organization

--- resources/js/widgets/admin/grid/MarketingGrid.svelte
+++ resources/js/widgets/admin/grid/MarketingGrid.svelte
@@ -33,8 +33,8 @@
         </div>
     </div>
 </Section>
-<Section title="Diversos">
-    <div class="flex justify-center mb-10">
+<Section title="Todos os conteúdos">
+    <div class="flex justify-center mt-5 mb-5">
         <Offcanvas>
             <div class="cursor-pointer w-full lg:w-auto py-2 px-6 border-4 border-solid border-orange-amber rounded-xl text-orange-amber text-xl font-bold font-noto-sans italic uppercase" slot="action" >
                 Upar conteúdo
