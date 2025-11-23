1b8f9b93d824a1f5e5ef7b398a5e85a05daf943a
ojgabrielleal <joaogabrieel79@gmail.com>
2025-11-22 21:39:49 -0300

    feat: create AdmsController to handle admin user management and render the admin view.

--- app/Http/Controllers/Web/Admin/AdmsController.php
+++ app/Http/Controllers/Web/Admin/AdmsController.php
@@ -18,23 +18,28 @@ use App\Models\Role;
 
 class AdmsController extends Controller
 {
-    use ProvideSuccessTrait, ProvideGenericExceptionTrait;
+    use ProvideSuccessTrait;
 
     public function listUsers()
     {
         try{
             $query = User::where('is_active', true);
-            $query->with('roles');
+            $query->with('role');
             $user = $query->get();
-            return [
-                'id' => $user->id,
-                'name' => $user->name,
-                'nickname' => $user->nickname,
-                'avatar' => $user->avatar,
-                'highest_role' => $user->highest_role,
-            ];
+
+            $map = $user->map(function($item){
+                return [
+                    'id' => $item->id,
+                    'username' => $item->username,
+                    'name' => $item->name,
+                    'nickname' => $item->nickname,
+                    'avatar' => $item->avatar,
+                ];
+            });
+
+            return $map;
         } catch (\Throwable $e) {
-            return $this->provideGenericException($e);
+            throw $e;
         }
     }
 
@@ -42,12 +47,18 @@ class AdmsController extends Controller
     {
         try{
             $query = User::where('id', $id);
-            $query->with(['userExternalLinks', 'userLikes', 'roles']);
+            $query->with(['userPreference', 'userSocial', 'role']);
             $user = $query->firstOrFail();
 
-            return $user;
+            return [
+                'id' => $user->id,
+                'username' => $user->username,
+                'name' => $user->name,
+                'nickname' => $user->nickname,
+                'avatar' => $user->avatar,
+            ];
         } catch (\Throwable $e) {
-            return $this->provideGenericException($e);
+            throw $e;
         }
     }
 
@@ -61,13 +72,6 @@ class AdmsController extends Controller
                 'nickname' => 'required',
                 'gender' => 'required',
                 'permission' => 'required'
-            ], [
-                'username.required' => 'Login',
-                'password.required' => 'Senha',
-                'name.required' => 'Nome',
-                'nickname.required' => 'Apelido',
-                'gender.required' => 'Gênero',
-                'permission.required' => "Cargo principal"
             ]);
 
             $exists = User::where('username', $request->input('username'))->exists();
@@ -83,10 +87,12 @@ class AdmsController extends Controller
                 'gender' => $request->input('gender')
             ]);
 
-            UserPermission::create([
-                'user_id' => $user->id,
-                'permission' => $request->input('permission')
-            ]);
+            if($user){
+                UserPermission::create([
+                    'user_id' => $user->id,
+                    'permission' => $request->input('permission')
+                ]);
+            }
 
             return $this->provideSuccess('save');
         } catch (\Throwable $e) {
